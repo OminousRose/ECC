3a9d23c259f0c91494dd317cb618e4de0b75c2ef
Eric Flores <eric.flores.cs@outlook.com>
2021-11-23 11:07:30 -0600

    Add lab 1 cpp

--- Computer Science 2/Lab Assignments/Lab 1/main.cpp
+++ Computer Science 2/Lab Assignments/Lab 1/main.cpp
@@ -1,40 +1,51 @@
+//    Name: Eric Flores
+//    Assignment: Lab 1
+//    Date: 11/22/2021  
+// 
+//    Description: This program allows a user to input a string and will make use of pointers
+//                 in order to reverse the string. The input string will then be displayed to the
+//                 console after the string is input.
+//
+//
 #include <iostream>
-#include <cstring>
+#include <cstdlib>
+#include <cctype>
+#include <string.h>
 
 using namespace std;
 
-string reverse(char word[100]);
+void reverse(char* s);
 
 int main()
 {
-cout << "This program will take a user input string and output the string in reverse." << endl;
+    char str[100] = "";                    // Set the character size to 100
+    cout << "This program reverses a user input string!" << endl;
+    cout << "Enter your string:" << endl;      // Prompt user to enter a string
+    cin.get(str, 100);                    // Using only cin >> str will only reverse the first part of the string if a space is used.
 
-char Rstring[100];
+    reverse(str);
 
-cout << "Enter a string: \n";
-cin >> Rstring;
+    cout << "The string reversed would be:" << endl;
+    cout << str << endl;                                // Output
 
-
-string result = reverse(Rstring);
-
-cout << result;
+    return 0;
 }
 
-string reverse(char word[100])
+void reverse(char* s)
 {
+    char* front, * rear, temp;              // Initialize *front, *rear, and a temp that will be used to assist in reversing
 
-    char a= 'a';
-    char b= 'b';
-
-    char *front=&a;
-    char *rear=&b;
-    for (int i=0; i<(strlen(word)/2);i++)
+    front = s;
+    rear = s + strlen(s) - 1;
+    while (front < rear)
     {
-        front[0]=word[i];
-            rear[0]=word[strlen(word)-1-i];
-            word[i]=*rear;
-            word[strlen(word)-1-i]=*front;
+
+        temp = *front;                      
+        *front = *rear;                     // During the while loop, the program uses a temp character to store the front character. So after the rear character is swapped to the front,
+        *rear = temp;                       // the rear character is replaced with the front character, which was stored as a temp. Then the process repeats until the whole string is reversed.
+
+        front++;                            // After the first character swaps, the code moves on to the next character to the right
+        rear--;                             // After the rear character swaps to the front, the code moves to the previous character to the left until can't procede past the front character.
     }
-        
-        return word;
-}   
+    return;
+}
