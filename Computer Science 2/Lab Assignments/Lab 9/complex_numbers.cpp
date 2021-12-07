3d4e0596d61ea4b7acbe2269b5ff659ecb6ae4b3
Eric Flores <eric.flores.cs@outlook.com>
2021-12-06 19:02:39 -0600

    Add lab 9 cpp

---
+++ Computer Science 2/Lab Assignments/Lab 9/complex_numbers.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+// class declaration
+class Complex
+{
+private:
+double real;
+double imaginary;
+  
+public:
+Complex(double realPart , double imagPart)
+{
+real = realPart ;
+imaginary = imagPart;
+}
+  
+  
+Complex(double realPart)
+{
+real = realPart ;
+imaginary = 0;
+}
+  
+Complex()
+{
+real = 0;
+imaginary = 0;
+}
+  
+Complex operator+(const Complex& b) {
+Complex c;
+c.real = this->real + b.real;
+c.imaginary = this->imaginary + b.imaginary;
+return c;
+}
+  
+Complex operator-(const Complex& b) {
+Complex c;
+c.real = this->real - b.real;
+c.imaginary = this->imaginary - b.imaginary;
+return c;
+}
+  
+Complex operator*(const Complex& b) {
+Complex c;
+c.real = this->real*b.real - this->imaginary*b.imaginary;
+c.imaginary = this->real*b.imaginary + this->imaginary*b.real;
+return c;
+}
+  
+bool operator==(const Complex& b) {
+return (this->real == b.real) && (this->imaginary == b.imaginary);
+}
+  
+friend ostream &operator << (ostream &os, Complex &t) {
+os << t.real << " + " << t.imaginary << "*i" << endl;
+return os;
+}
+  
+friend istream & operator >> (istream &in, Complex &c)
+{
+cout << "Enter Real Part ";
+in >> c.real;
+cout << "Enter Imagenory Part ";
+in >> c.imaginary;
+return in;
+}
+};
--- Computer Science 2/Lab Assignments/Lab 9/main.cpp
+++ Computer Science 2/Lab Assignments/Lab 9/main.cpp
@@ -9,25 +9,38 @@
 //                 Definitions for the member functions of the class are written outside the class on a different file.
 //
 #include <iostream>
-#include "complex.cpp"
+#include "complex_numbers.cpp"
 
 using namespace std;
 
-
 int main()
 {
-Complex c1(2,3);
-Complex c2(10,11);
-Complex c3 = c1 + c2;
-cout << "Addition : " << c3 << endl;
-Complex c4 = c2 - c1;
-cout << "Subtraction : " << c4 << endl;
-Complex c5 = c1 * c2;
-cout << "Multiplication : " << c5 << endl;
-Complex c6(10,11);
-bool varBool = c6 == c1;
-cout << "Are c6 and c2 equal:" << varBool << endl;
-bool varBool1 = c6 == c2;
-cout << "Are c6 and c2 equal:" << varBool1 << endl;
-return 0;
+Complex c1;
+Complex c2;
+  
+cin >> c1;
+cin >> c2;
+
+cout << c1;
+cout << c2;
+
+Complex c3;
+c3 = c1+c2;
+
+cout << c3;
+
+c3 = c1-c2;
+cout << c3;
+
+c3 = c1*c2;
+cout << c3;
+
+if (c1 == c2)
+{
+cout << "Equal\n";
+}   
+else
+{
+cout <<"Not equal\n";
+}
 }
