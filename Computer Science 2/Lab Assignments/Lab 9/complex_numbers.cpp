32d3b33e51c10e6180b4b65ee6de072872c294fb
Eric Flores <eric.flores.cs@outlook.com>
2021-12-06 19:15:18 -0600

    Add lab 9 cpp

--- Computer Science 2/Lab Assignments/Lab 9/complex_numbers.cpp
+++ Computer Science 2/Lab Assignments/Lab 9/complex_numbers.cpp
@@ -8,6 +8,7 @@ double real;
 double imaginary;
   
 public:
+// real part and imaginary part are stored as double values
 Complex(double realPart , double imagPart)
 {
 real = realPart ;
@@ -26,32 +27,32 @@ Complex()
 real = 0;
 imaginary = 0;
 }
-  
+ // overload addition operator 
 Complex operator+(const Complex& b) {
 Complex c;
 c.real = this->real + b.real;
 c.imaginary = this->imaginary + b.imaginary;
 return c;
 }
-  
+// overload subraction operator
 Complex operator-(const Complex& b) {
 Complex c;
 c.real = this->real - b.real;
 c.imaginary = this->imaginary - b.imaginary;
 return c;
 }
-  
+// overload multiplication operator 
 Complex operator*(const Complex& b) {
 Complex c;
 c.real = this->real*b.real - this->imaginary*b.imaginary;
 c.imaginary = this->real*b.imaginary + this->imaginary*b.real;
 return c;
 }
-  
+// overload == operator  
 bool operator==(const Complex& b) {
 return (this->real == b.real) && (this->imaginary == b.imaginary);
 }
-  
+// overload ostream and istream operators  
 friend ostream &operator << (ostream &os, Complex &t) {
 os << t.real << " + " << t.imaginary << "*i" << endl;
 return os;
