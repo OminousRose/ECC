dd548ec4316520a81f331b74f36d089fc044622f
Eric Flores <eric.flores.cs@outlook.com>
2021-12-11 17:40:45 -0600

    Add lab 8 header

--- Computer Science 2/Lab Assignments/Lab 8/rational.h
+++ Computer Science 2/Lab Assignments/Lab 8/rational.h
@@ -3,7 +3,8 @@
 #include <iostream>
 using namespace std;
 
-class Rational {
+class Rational
+{
     // class rational declaration
     int numerator, denominator;
     // define a class called Rational that would be used to store rational
@@ -13,31 +14,30 @@ class Rational {
     // operators =, +=, -=, *=, /=, and then using these operators overload the
     // following operators: +, -, *, /. Also overload the comparison operators <, >,
     // ==, !=, <=, >= and the i/o stream operators >> and <<.
-    public:
+public:
     Rational();
     Rational(int num, int den);
-    Rational(const Rational& r);
-    int get_numerator() {return numerator;}
-    int get_denominator() {return denominator;}
+    Rational(const Rational &r);
+    int get_numerator() { return numerator; }
+    int get_denominator() { return denominator; }
     void simplify();
-    Rational& operator=(const Rational& r);
-    Rational& operator+=(const Rational& r);
-    Rational& operator-=(const Rational& r);
-    Rational& operator*=(const Rational& r);
-    Rational& operator/=(const Rational& r);
-    Rational operator+(const Rational& r);
-    Rational operator-(const Rational& r);
-    Rational operator*(const Rational& r);
-    Rational operator/(const Rational& r);
-    bool operator<(const Rational& r);
-    bool operator>(const Rational& r);
-    bool operator==(const Rational& r);
-    bool operator!=(const Rational& r);
-    bool operator<=(const Rational& r);
-    bool operator>=(const Rational& r);
-    friend ostream& operator<<(ostream& os, const Rational& r);
-    friend istream& operator>>(istream& is, Rational& r);
-
+    Rational &operator=(const Rational &r);
+    Rational &operator+=(const Rational &r);
+    Rational &operator-=(const Rational &r);
+    Rational &operator*=(const Rational &r);
+    Rational &operator/=(const Rational &r);
+    Rational operator+(const Rational &r);
+    Rational operator-(const Rational &r);
+    Rational operator*(const Rational &r);
+    Rational operator/(const Rational &r);
+    bool operator<(const Rational &r);
+    bool operator>(const Rational &r);
+    bool operator==(const Rational &r);
+    bool operator!=(const Rational &r);
+    bool operator<=(const Rational &r);
+    bool operator>=(const Rational &r);
+    friend ostream &operator<<(ostream &os, const Rational &r);
+    friend istream &operator>>(istream &is, Rational &r);
 };
 
 #endif
