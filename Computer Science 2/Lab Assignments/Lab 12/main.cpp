d31a9d0e70c80a1adbb4ef44c6c0e43f13078807
Eric Flores <eric.flores.cs@outlook.com>
2021-12-13 13:53:35 -0600

    Add lab 12 cpp

--- Computer Science 2/Lab Assignments/Lab 12/main.cpp
+++ Computer Science 2/Lab Assignments/Lab 12/main.cpp
@@ -1,3 +1,14 @@
+//    Name: Eric Flores
+//    Assignment: Lab 12
+//    Title:      Bank Account
+//    Date: 12/13/2021
+//
+//    Description: This short program assumes the existence of a "BankAccount" class to derive a class "SavingsAccount"
+//                 It contains two instance variables: a double declared as "interestRate" and an int declared as "interestType"
+//                 interestType can have two values 1 or 2, which are labeled "Simple" or "Compound" respectively.
+//                 The interestType variable is initialized by a strings "Simple" or "Compound"
+//                 In the end, two pairs of functions "getInterestRate" and "getInterestType" return the values
+//                 of the correspondingh data members as double and int respectively.
 class SavingsAccount : public BankAccount
 {
 
