924bc07dc0ab5bc2f9dbfefd2a1ff0ea59225385
Eric Flores <eric.flores.cs@outlook.com>
2021-12-14 18:55:26 -0600

    Add week 13 assignment cpp

---
+++ Computer Science 2/Homework/Week 13 Assignment/main.cpp
@@ -0,0 +1,33 @@
+// Week 13 Assignment
+// Name: Eric Flores
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // declare int for integer data type values
+    int dollars, remainingCoupons;
+    int noChocolate, addi_Chocolate;
+
+    cout << "Enter your amount of dollars: ";
+    cin >> dollars;
+
+    // initialize noChocolate and remainingCoupons
+    noChocolate = dollars;
+    remainingCoupons = noChocolate;
+
+    while (remainingCoupons >= 7)
+    {
+        // statement to calculate the noChocolate,
+        // additional chocolate and remaining coupons
+        addi_Chocolate = remainingCoupons / 7;
+        noChocolate = noChocolate + addi_Chocolate;
+        remainingCoupons = (remainingCoupons % 7) + addi_Chocolate;
+    }
+
+    // output
+    cout << "Total number of chocolate bars is: " << noChocolate << endl;
+    cout << "Number of leftover coupons is: " << remainingCoupons << endl;
+
+    return 0;
+}
