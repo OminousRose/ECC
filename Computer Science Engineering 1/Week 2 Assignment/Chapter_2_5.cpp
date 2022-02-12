a203caa0e8b5700aa4565df77712db0261c177b9
Eric Flores <eric.flores.cs@outlook.com>
2022-02-11 18:20:57 -0600

    Add chapter 2 assignment

---
+++ Computer Science Engineering 1/Week 2 Assignment/Chapter_2_5.cpp
@@ -0,0 +1,30 @@
+/*
+Name: Eric Flores
+Date: 2/11/2022
+Assignment: Chapter 2_5 Program
+Description: Week 2 assignment requires completion and execution of example program 2_5 on page 74 in the textbook.
+*/
+
+/* Program chapter2_5 */
+/*
+/* This program computes area of a circle. */
+/* Results are displayed with two digits */
+/* to the right of the decimal point. */
+#include <iostream> //Required for cout, endl.
+#include <iomanip>  //Required for setw() setprecision(), fixed.
+#include <cmath>    //Required for acos().
+using namespace std;
+const double PI = acos(-1.0);
+int main()
+{
+    //Declare and initialize objects,
+    double radius{10}, area;
+    area = PI * radius * radius;
+
+    cout << fixed << setprecision(2);
+    cout << "The radius of the circle is: "
+         << setw(10) << radius << "  centimeters" << endl;
+    cout << "The area of the circle is:  " << setw(10) << area << " square centimeters" << endl;
+
+    // Exit program
+    return 0;
