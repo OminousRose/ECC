53a45678995d00676e3b564553f60fb3f84e83ec
Eric Flores <eric.flores.cs@outlook.com>
2022-03-11 14:21:32 -0600

    Add week 6 assignment

---
+++ Computer Science Engineering 1/Week 6 Assignment/main.cpp
@@ -0,0 +1,24 @@
+/*
+Name: Eric Flores
+Date: 3/11/2022
+Assignment: Week 6 Program
+Description: Complete Problem 20 from Chapter 4 in the book. Submit screenshot and .cpp file
+Problem:  Generate a table of conversions from mph to ft/s. Start the mph column at 0 and
+increment by 5 mph. The last line should contain the value 65 mph. (Recall that
+1 mi = 5,280 ft.)
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float MPH, FPS;
+    cout << "MPH" << '\t' << "res" << endl;
+
+    for (MPH = 0; MPH <= 65; MPH = MPH + 5;)
+    {
+        FPS = (MPH * 5280 / 3600);
+        cout << MPH << '\t' << float(FPS) << endl;
+    }
+    return 0;
+}
